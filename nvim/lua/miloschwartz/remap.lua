a4e53c59499dcd93156c1b8c9fc6338606491071
Milo Schwartz <mschwartz10612@gmail.com>
2024-01-03 16:49:40 -0500

    added terminal escape keymap

--- nvim/lua/miloschwartz/remap.lua
+++ nvim/lua/miloschwartz/remap.lua
@@ -12,17 +12,15 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "{", "{zz")
 vim.keymap.set("n", "}", "}zz")
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- escape terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
