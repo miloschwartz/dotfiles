cc446294b1e26f0ceaa777ca8a8012fd4452a791
Milo Schwartz <mschwartz10612@gmail.com>
2024-01-28 00:16:54 -0500

    added peristent undo

--- nvim/lua/miloschwartz/init.lua
+++ nvim/lua/miloschwartz/init.lua
@@ -24,3 +24,17 @@ vim.opt.foldlevel = 99
 
 -- Enable Markdown folding
 vim.g.markdown_folding = 1
+
+-- Create a directory for undo files
+local undodir = vim.fn.expand('~/.undo')
+
+-- Check if the directory exists, if not create it
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, 'p')
+end
+
+-- Set up the undo directory
+vim.opt.undodir = undodir
+
+-- Enable persistent undo
+vim.opt.undofile = true
