133dbbd981af2f67096553c4cb31efa5baf0c17d
miloschwartz <mschwartz10612@gmail.com>
2025-11-07 22:32:26 -0800

    clean up packer

--- nvim/lua/miloschwartz/packer.lua
+++ nvim/lua/miloschwartz/packer.lua
@@ -2,23 +2,19 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use {
-        'wbthomason/packer.nvim',
-        -- commit = "ea0cc3c59f67c440c5ff0bbe4fb9420f4350b9a3"
+        'wbthomason/packer.nvim'
     }
 
     use {
-        'nvim-lua/plenary.nvim',
-        -- commit = "8aad4396840be7fc42896e3011751b7609ca4119"
+        'nvim-lua/plenary.nvim'
     }
 
     use {
-        "nvim-telescope/telescope-live-grep-args.nvim",
-        -- commit = "731a046da7dd3adff9de871a42f9b7fb85f60f47"
+        "nvim-telescope/telescope-live-grep-args.nvim"
     }
 
     use {
         'nvim-telescope/telescope.nvim',
-        -- commit = "5a701e99906961218b55d7ad6c2a998f066c6fe0",
         after = {
             "telescope-live-grep-args.nvim",
             'plenary.nvim'
@@ -27,20 +23,17 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-        -- commit = 'ef267f0c285928ea3a0d3362a260a0728fd4a146'
+        run = ':TSUpdate'
     }
 
     use {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
-        after = "plenary.nvim",
-        -- commit = "0378a6c428a0bed6a2781d459d7943843f374bce"
+        after = "plenary.nvim"
     }
 
     use {
-        'mbbill/undotree',
-        -- commit = "aa93a7e5890dbbebbc064cd22260721a6db1a196"
+        'mbbill/undotree'
     }
 
     use {
@@ -48,167 +41,105 @@ return require('packer').startup(function(use)
         branch = 'v3.x',
         requires = {
             {
-                'williamboman/mason.nvim',
-                -- commit = "751b1fcbf3d3b783fcf8d48865264a9bcd8f9b10"
+                'williamboman/mason.nvim'
             },
             {
-                'williamboman/mason-lspconfig.nvim',
-                -- commit = "44509689b9bf3984d729cc264aacb31cb7f41668"
+                'williamboman/mason-lspconfig.nvim'
             },
             {
-                'neovim/nvim-lspconfig',
-                -- commit = "b3014f2209503944f2714cf27c95591433a0c7d8"
+                'neovim/nvim-lspconfig'
             },
             {
-                'hrsh7th/nvim-cmp',
-                -- commit = "ce16de5665c766f39c271705b17fff06f7bcb84f"
+                'hrsh7th/nvim-cmp'
             },
             {
-                'hrsh7th/cmp-nvim-lsp',
-                -- commit = "5af77f54de1b16c34b23cba810150689a3a90312"
+                'hrsh7th/cmp-nvim-lsp'
             },
             {
-                'L3MON4D3/LuaSnip',
-                -- commit = "be7be2ca7f55bb881a7ffc16b2efa5af034ab06b"
+                'L3MON4D3/LuaSnip'
             },
         }
     }
 
     use {
-        'sbdchd/neoformat',
-        -- commit = "4372abb846f43ec121df40e620682c985ebc8286"
+        'sbdchd/neoformat'
     }
 
     use {
         'github/copilot.vim',
-        branch = 'release',
-        -- commit = "1e135c5303bc60598f6314a2276f31dc91aa34dd"
-    }
-
-    -- use {
-    --     'alvan/vim-closetag',
-    --     commit = "d0a562f8bdb107a50595aefe53b1a690460c3822"
-    -- }
-
-    use {
-        "windwp/nvim-autopairs",
-       -- commit = "4f41e5940bc0443fdbe5f995e2a596847215cd2a"
+        branch = 'release'
     }
 
     use {
-        'rlane/pounce.nvim',
-        -- commit = "0c044cad69571d57d8f64a41cca95332859b6abc"
+        "windwp/nvim-autopairs"
     }
 
     use {
-        'dmmulroy/tsc.nvim',
-        -- commit = "2576637ce46aed84240da33496eee6430b0f0248"
-    }
-
-    -- use {
-    --     "ray-x/lsp_signature.nvim",
-    --     -- commit = "c6aeb2f1d2538bbdfdaab1664d9d4c3c75aa9db8"
-    -- }
-
-    -- use {
-    --     'nvim-tree/nvim-web-devicons',
-    --     commit = "6e355632387a085f15a66ad68cf681c1d7374a04"
-    -- }
-
-    -- use {
-    --     'nvim-lualine/lualine.nvim',
-    --     after = 'nvim-web-devicons',
-    --     commit = "0a5a66803c7407767b799067986b4dc3036e1983"
-    -- }
-
-    use {
-        'unblevable/quick-scope',
-        -- commit = "256d81e391a22eeb53791ff62ce65f870418fa71"
+        'rlane/pounce.nvim'
     }
 
     use {
-        'tomtom/tcomment_vim',
-        -- commit = "48ab639a461d9b8344f7fee06cb69b4374863b13"
+        'dmmulroy/tsc.nvim'
     }
 
     use {
-        "kylechui/nvim-surround",
-        -- commit = "a4e30d33add8a9743b4f518b3a788b3c8e5def71"
+        'unblevable/quick-scope'
+    }
+
+    use {
+        'tomtom/tcomment_vim'
+    }
+
+    use {
+        "kylechui/nvim-surround"
     }
 
     use {
         "nvim-treesitter/nvim-treesitter-textobjects",
-        after = "nvim-treesitter",
-        -- commit = "c180aef9a197e9fe64cc285171910b8ea1400952"
+        after = "nvim-treesitter"
     }
 
     use {
-        "aznhe21/actions-preview.nvim",
-        -- commit = "5072b1b1065a6b22bdd46b5c21780a91d6a08071"
+        "aznhe21/actions-preview.nvim"
     }
 
     use {
-        "stevearc/oil.nvim",
-        -- commit = "e462a3446505185adf063566f5007771b69027a1"
+        "stevearc/oil.nvim"
     }
 
     use {
-        'nvim-tree/nvim-tree.lua',
-        -- commit = "81eb8d519233c105f30dc0a278607e62b20502fd"
+        'nvim-tree/nvim-tree.lua'
     }
 
     use {
-        "lewis6991/gitsigns.nvim",
-        -- commit = "d96ef3bbff0bdbc3916a220f5c74a04c4db033f2"
+        "lewis6991/gitsigns.nvim"
     }
 
     use {
-        "echasnovski/mini.nvim",
-        -- commit = "e3d13e2c89a9dfda51c69c825360d1fe5be0e322"
+        "echasnovski/mini.nvim"
     }
 
     use {
-        "tpope/vim-fugitive",
-        -- commit = "dac8e5c2d85926df92672bf2afb4fc48656d96c7"
+        "tpope/vim-fugitive"
     }
 
     use {
-        "nvim-pack/nvim-spectre",
-        -- commit = "9653847cf2f225648967f6e9363643e327387579"
+        "nvim-pack/nvim-spectre"
     }
 
 	use {
-		"christoomey/vim-tmux-navigator",
-		-- commit = "5b3c701686fb4e6629c100ed32e827edf8dad01e"
+		"christoomey/vim-tmux-navigator"
 	}
 
 	use {
-		"epwalsh/obsidian.nvim",
-		-- commit = "14e0427bef6c55da0d63f9a313fd9941be3a2479"
+		"epwalsh/obsidian.nvim"
 	}
 
-	-- use {
-	-- 	"onsails/lspkind-nvim",
-	-- 	commit = "cff4ae321a91ee3473a92ea1a8c637e3a9510aec"
-	-- }
-
-	-- use {
-	-- 	"rose-pine/neovim",
-	-- 	-- commit = "d396005db5bbd1d4ec7772a7c96c96f4c4802328"
-	-- }
-
-	use ({
-		"projekt0n/github-nvim-theme",
-		-- commit = "0e4636f556880d13c00d8a8f686fae8df7c9845f"
-	})
-
-	-- use {
-    --     "lukas-reineke/indent-blankline.nvim",
-    --     commit = "3d08501caef2329aba5121b753e903904088f7e6"
-    -- }
+	use {
+		"projekt0n/github-nvim-theme"
+	}
 
 	use {
-		"tpope/vim-sleuth",
-		-- commit = "be69bff86754b1aa5adcbb527d7fcd1635a84080"
+		"tpope/vim-sleuth"
 	}
 end)
